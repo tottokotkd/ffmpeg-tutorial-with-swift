1d1459ed61a6335fb6c4329e3ad0dce5baa9f460
Tokuda Shusuke <tottokotkd@tottokoMBP.local>
2015-01-22 14:20:43 +0900

    Initial Commit

---
+++ Tutorials/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Tutorials
+//
+//  Created by Tokuda Shusuke on 2015/01/22.
+//  Copyright (c) 2015年 Tokuda Shusuke. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
